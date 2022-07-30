798e6360666970341201b09ba7337671319de051
Holger <hdecarne@gmail.com>
2022-07-30 21:45:04 +0200

    Rework conn type

--- conn.go
+++ conn.go
@@ -8,6 +8,7 @@
 package nsdp
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"log"
@@ -17,28 +18,24 @@ import (
 	"time"
 )
 
-// Conn represents a network connection used for sending and receiving NSDP message.
+// Conn represents a network connection used for sending and receiving NSDP messages.
 type Conn struct {
 	laddr              *net.UDPAddr
 	taddr              *net.UDPAddr
-	broadcast          bool
 	host               net.HardwareAddr
 	conn               *net.UDPConn
-	seq                uint16
-	RetryCount         int           // Number of retries during message sending
-	RetryInterval      time.Duration // Interval after which message sending is retried
-	ReceiveBufferSize  uint          // Receive buffer size
-	ReceiveQueueLength uint          // Receive queue length
-	Debug              bool
+	seq                Sequence
+	ReceiveBufferSize  uint // Receive buffer size
+	ReceiveQueueLength uint // Receive queue length
+	ReceiveTimeout     time.Duration
+	Debug              bool // Enables debug output via log.Printf
 }
 
-// NewConn establishes a new connection to the given remote endpoint.
-//
-// The target endpoint can be a concrete device or a broadcast address.
-func NewConn(target string, broadcast bool, debug bool) (*Conn, error) {
+// NewConn establishes a new connection to the given remote target.
+func NewConn(target string, debug bool) (*Conn, error) {
 	if debug {
 		log.Printf("NSDP setting up connection...")
-		log.Printf("NSDP target address: '%s' (broadcast: %t)", target, broadcast)
+		log.Printf("NSDP target address: '%s'", target)
 	}
 	taddr, err := net.ResolveUDPAddr("udp", target)
 	if err != nil {
@@ -69,108 +66,147 @@ func NewConn(target string, broadcast bool, debug bool) (*Conn, error) {
 	if debug {
 		log.Printf("NSDP host MAC: '%s'", host)
 	}
-	lconn, err := net.ListenUDP("udp", laddr)
+	conn, err := net.ListenUDP("udp", laddr)
 	if err != nil {
 		return nil, err
 	}
 	return &Conn{
 		laddr:              laddr,
 		taddr:              taddr,
-		broadcast:          broadcast,
 		host:               host,
-		conn:               lconn,
-		seq:                uint16(time.Now().Unix()),
-		RetryCount:         3,
-		RetryInterval:      time.Millisecond * 5000,
+		conn:               conn,
+		seq:                Sequence(time.Now().UnixNano()),
 		ReceiveBufferSize:  8192,
-		ReceiveQueueLength: 10,
+		ReceiveQueueLength: 16,
+		ReceiveTimeout:     time.Millisecond * 2000,
 		Debug:              debug,
 	}, nil
 }
 
-// Close closes the connection
+// Close closes the connection.
 func (c *Conn) Close() error {
 	return c.conn.Close()
 }
 
-type receiveQueueEntry struct {
-	buffer []byte
-	len    int
-	addr   *net.UDPAddr
-	err    error
-}
-
-// SendReceiveMessage sends the given NSDP message and waits for the responses.
+// SendReceiveMessage sends the given NSDP message and waits for responses.
 //
 // Prior to sending the message's host address and sequence number is updated according to the current connection state.
-// If the broadcast flag has been set during connection creation, an arbitrary number of response message is returned.
-// If the broadcast flag has not been set, exactly one response message is returned.
-func (c *Conn) SendReceiveMessage(msg *Message) ([]*Message, error) {
-	msg.HostAddress = c.host
-	receiveQueue := make(chan receiveQueueEntry, c.ReceiveQueueLength)
+// If the message's device address is empty, an arbitrary number of response message is returned.
+// If the message's device address has been set, exactly one response message is returned.
+func (c *Conn) SendReceiveMessage(msg *Message) (map[string]*Message, error) {
+	c.seq += 1
+	c.conn.SetReadDeadline(time.Now().Add(c.ReceiveTimeout))
+	if bytes.Equal(msg.Header.DeviceAddress, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00}) {
+		return c.sendReceiveBroadcastMessage(msg)
+	}
+	return c.sendReceiveUnicastMessage(msg)
+}
+
+type receiveQueueEntry struct {
+	msg *Message
+	err error
+}
+
+func (c *Conn) sendReceiveBroadcastMessage(msg *Message) (map[string]*Message, error) {
+	receiveQueue := make(chan *receiveQueueEntry, c.ReceiveQueueLength)
 	go func() {
 		for {
-			entry := receiveQueueEntry{
-				buffer: make([]byte, c.ReceiveBufferSize),
+			msg, err := c.receiveMessage()
+			receiveQueue <- &receiveQueueEntry{
+				msg: msg,
+				err: err,
 			}
-			entry.len, entry.addr, entry.err = c.conn.ReadFromUDP(entry.buffer)
-			receiveQueue <- entry
-			if !c.broadcast {
+			if err != nil {
 				break
 			}
 		}
 	}()
-	retry := 0
-	retryTicker := time.NewTicker(c.RetryInterval)
-	defer retryTicker.Stop()
-	receivedMsgs := make([]*Message, 0)
-	msg.Sequence = c.seq
-	c.seq = c.seq + 1
-	for retry < c.RetryCount {
-		if msg.Sequence != c.seq {
-			msg.Sequence = c.seq
-			sendBuffer := msg.Marshal()
-			if c.Debug {
-				log.Printf("NSDP %s > %s:\n%s\n%s", c.laddr, c.taddr, hex.EncodeToString(sendBuffer), msg)
+	err := c.sendMessage(msg)
+	if err != nil {
+		return nil, err
+	}
+	receivedMsgs := make(map[string]*Message, 0)
+	for {
+		received := <-receiveQueue
+		if received.err != nil {
+			if nerr, ok := received.err.(net.Error); ok && nerr.Timeout() {
+				break
 			}
-			_, err := c.conn.WriteToUDP(sendBuffer, c.taddr)
-			if err != nil {
-				fmt.Println(err)
-			}
+			return nil, received.err
 		}
-		select {
-		case received := <-receiveQueue:
-			if received.err == nil {
-				receivedMsg, err := UnmarshalMessage(received.buffer[:received.len])
-				if err == nil {
-					if c.Debug {
-						log.Printf("NSDP %s < %s:\n%s\n%s", c.laddr, received.addr, hex.EncodeToString(received.buffer[:received.len]), receivedMsg)
-					}
-					receivedMsgs = append(receivedMsgs, receivedMsg)
-				} else {
-					if c.Debug {
-						log.Printf("NSDP %s < %s:\n%s", c.laddr, received.addr, hex.EncodeToString(received.buffer[:received.len]))
-						log.Printf("NSDP Error while unmarshaling message; cause: %v", err)
-					}
-				}
-			} else {
-				if c.Debug {
-					log.Printf("NSDP listen error: %v", received.err)
-				}
-			}
-		case <-retryTicker.C:
-			if len(receivedMsgs) != 0 {
-				retry = c.RetryCount
-			} else {
-				c.seq = c.seq + 1
-				retry = retry + 1
-			}
-		}
+		receivedMsgs[received.msg.Header.DeviceAddress.String()] = received.msg
 	}
 	return receivedMsgs, nil
 }
 
+func (c *Conn) sendReceiveUnicastMessage(msg *Message) (map[string]*Message, error) {
+	receiveQueue := make(chan *receiveQueueEntry, 1)
+	go func() {
+		for {
+			msg, err := c.receiveMessage()
+			receiveQueue <- &receiveQueueEntry{
+				msg: msg,
+				err: err,
+			}
+			break
+		}
+	}()
+	err := c.sendMessage(msg)
+	if err != nil {
+		return nil, err
+	}
+	receivedMsgs := make(map[string]*Message, 0)
+	received := <-receiveQueue
+	if received.err != nil {
+		return nil, received.err
+	}
+	receivedMsgs[received.msg.Header.DeviceAddress.String()] = received.msg
+	return receivedMsgs, nil
+}
+
+func (c *Conn) sendMessage(msg *Message) error {
+	preparedMsg := msg.prepareMessage(c.host, c.seq)
+	sendBuffer := preparedMsg.Marshal()
+	if c.Debug {
+		log.Printf("NSDP %s > %s:\n%s\n%s", c.laddr, c.taddr, hex.EncodeToString(sendBuffer), preparedMsg)
+	}
+	_, err := c.conn.WriteToUDP(sendBuffer, c.taddr)
+	return err
+}
+
+func (c *Conn) receiveMessage() (*Message, error) {
+	buffer := make([]byte, c.ReceiveBufferSize)
+	for {
+		len, addr, err := c.conn.ReadFromUDP(buffer)
+		if err != nil {
+			return nil, err
+		}
+		msg, err := UnmarshalMessage(buffer[:len])
+		if err != nil {
+			if c.Debug {
+				log.Printf("NSDP %s < %s:\n%s", c.laddr, addr, hex.EncodeToString(buffer[:len]))
+				log.Printf("NSDP Error while unmarshaling message; cause: %v", err)
+			}
+			return nil, err
+		}
+		if msg.Header.Sequence == c.seq {
+			if c.Debug {
+				log.Printf("NSDP %s < %s:\n%s\n%s", c.laddr, addr, hex.EncodeToString(buffer[:len]), msg)
+			}
+			return msg, nil
+		} else {
+			if c.Debug {
+				log.Printf("NSDP %s < %s:\nIgnoring unsolicted message (sequence: %04xh)", c.laddr, addr, msg.Header.Sequence)
+			}
+		}
+	}
+}
+
 func lookupHardwareAddr(addr *net.UDPAddr) (net.HardwareAddr, error) {
+	// lo has no real MAC
+	if addr.IP.IsLoopback() {
+		return make([]byte, 6), nil
+	}
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		return nil, err
