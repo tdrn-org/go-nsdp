ace5092c670adcd5dc24814383b2ae9e16dd3152
Holger <hdecarne@gmail.com>
2022-07-31 11:58:49 +0200

    Update file comment

--- nsdp.go
+++ nsdp.go
@@ -3,7 +3,7 @@
 // Copyright (C) 2022 Holger de Carne
 //
 // This software may be modified and distributed under the terms
-// of the MIT license.  See the LICENSE file for details.
+// of the MIT license. See the LICENSE file for details.
 
 /*
 This package provides support for the NSDP (Netgear Switch Discovery Protocol).
