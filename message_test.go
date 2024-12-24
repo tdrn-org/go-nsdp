f0310ad1623be57224902721e4bf1b9ee9998fb1
Holger <hdecarne@gmail.com>
2024-12-24 17:14:16 +0100

    Clean-up test code

--- message_test.go
+++ message_test.go
@@ -5,7 +5,7 @@
 // This software may be modified and distributed under the terms
 // of the MIT license. See the LICENSE file for details.
 
-package nsdp
+package nsdp_test
 
 import (
 	"crypto/rand"
@@ -14,130 +14,131 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/tdrn-org/go-nsdp"
 )
 
 func TestDeviceModelMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewDeviceModel("Model"))
+	runMessageMarshalingTest(t, nsdp.NewDeviceModel("Model"))
 }
 
 func TestDeviceModelString(t *testing.T) {
-	runMessageStringTest(t, NewDeviceModel("Model"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceModel(0001h) 'Model'\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewDeviceModel("Model"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceModel(0001h) 'Model'\nEOM   : ffff0000h")
 }
 
 func TestDeviceNameMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewDeviceName("Name"))
+	runMessageMarshalingTest(t, nsdp.NewDeviceName("Name"))
 }
 
 func TestDeviceNameString(t *testing.T) {
-	runMessageStringTest(t, NewDeviceName("Name"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceName(0003h) 'Name'\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewDeviceName("Name"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceName(0003h) 'Name'\nEOM   : ffff0000h")
 }
 
 func TestDeviceMAClMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewDeviceMAC(getRandomMAC()))
+	runMessageMarshalingTest(t, nsdp.NewDeviceMAC(getRandomMAC()))
 }
 
 func TestDeviceMACString(t *testing.T) {
-	runMessageStringTest(t, NewDeviceMAC(getStaticMAC()), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceMAC(0004h) 01:02:03:04:05:06\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewDeviceMAC(getStaticMAC()), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceMAC(0004h) 01:02:03:04:05:06\nEOM   : ffff0000h")
 }
 
 func TestDeviceLocationlMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewDeviceLocation("DeviceLocation"))
+	runMessageMarshalingTest(t, nsdp.NewDeviceLocation("DeviceLocation"))
 }
 
 func TestDeviceLocationString(t *testing.T) {
-	runMessageStringTest(t, NewDeviceLocation("Location"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceLocation(0005h) 'Location'\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewDeviceLocation("Location"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceLocation(0005h) 'Location'\nEOM   : ffff0000h")
 }
 
 func TestDeviceIPMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewDeviceIP(getRandomIP()))
+	runMessageMarshalingTest(t, nsdp.NewDeviceIP(getRandomIP()))
 }
 
 func TestDeviceIPString(t *testing.T) {
-	runMessageStringTest(t, NewDeviceIP(getStaticIP()), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceIP(0006h) 1.2.3.4\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewDeviceIP(getStaticIP()), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceIP(0006h) 1.2.3.4\nEOM   : ffff0000h")
 }
 
 func TestDeviceNetmaskMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewDeviceNetmask(getRandomIP()))
+	runMessageMarshalingTest(t, nsdp.NewDeviceNetmask(getRandomIP()))
 }
 
 func TestDeviceNetmaskString(t *testing.T) {
-	runMessageStringTest(t, NewDeviceNetmask(getStaticIP()), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceNetmask(0007h) 1.2.3.4\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewDeviceNetmask(getStaticIP()), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DeviceNetmask(0007h) 1.2.3.4\nEOM   : ffff0000h")
 }
 
 func TestRouterIPMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewRouterIP(getRandomIP()))
+	runMessageMarshalingTest(t, nsdp.NewRouterIP(getRandomIP()))
 }
 
 func TestRouterIPString(t *testing.T) {
-	runMessageStringTest(t, NewRouterIP(getStaticIP()), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: RouterIP(0008h) 1.2.3.4\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewRouterIP(getStaticIP()), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: RouterIP(0008h) 1.2.3.4\nEOM   : ffff0000h")
 }
 
 func TestDHCPModeMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewDHCPMode(1))
+	runMessageMarshalingTest(t, nsdp.NewDHCPMode(1))
 }
 
 func TestDHCPModeString(t *testing.T) {
-	runMessageStringTest(t, NewDHCPMode(1), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DHCPMode(000bh) Enabled\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewDHCPMode(1), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: DHCPMode(000bh) Enabled\nEOM   : ffff0000h")
 }
 
 func TestFWVersionSlot1Marshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewFWVersionSlot1("1.2.3.4"))
+	runMessageMarshalingTest(t, nsdp.NewFWVersionSlot1("1.2.3.4"))
 }
 
 func TestFWVersionSlot1String(t *testing.T) {
-	runMessageStringTest(t, NewFWVersionSlot1("1.2.3.4"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: FWVersionSlot1(000dh) '1.2.3.4'\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewFWVersionSlot1("1.2.3.4"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: FWVersionSlot1(000dh) '1.2.3.4'\nEOM   : ffff0000h")
 }
 
 func TestFWVersionSlot2Marshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewFWVersionSlot2("4.3.2.1"))
+	runMessageMarshalingTest(t, nsdp.NewFWVersionSlot2("4.3.2.1"))
 }
 
 func TestFWVersionSlot2String(t *testing.T) {
-	runMessageStringTest(t, NewFWVersionSlot2("4.3.2.1"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: FWVersionSlot2(000eh) '4.3.2.1'\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewFWVersionSlot2("4.3.2.1"), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: FWVersionSlot2(000eh) '4.3.2.1'\nEOM   : ffff0000h")
 }
 
 func TestPortStatusMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewPortStatus(1, 2))
+	runMessageMarshalingTest(t, nsdp.NewPortStatus(1, 2))
 }
 
 func TestPortStatusString(t *testing.T) {
-	runMessageStringTest(t, NewPortStatus(1, 2), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: PortStatus(0c00h) Port1 Status: 10Mbit/full-duplex Unknown1: 00h\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewPortStatus(1, 2), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: PortStatus(0c00h) Port1 Status: 10Mbit/full-duplex Unknown1: 00h\nEOM   : ffff0000h")
 }
 
 func TestPortStatisticMarshaling(t *testing.T) {
-	runMessageMarshalingTest(t, NewPortStatistic(1, 2, 3, 4, 5, 6, 7))
+	runMessageMarshalingTest(t, nsdp.NewPortStatistic(1, 2, 3, 4, 5, 6, 7))
 }
 
 func TestPortStatisticString(t *testing.T) {
-	runMessageStringTest(t, NewPortStatistic(1, 2, 3, 4, 5, 6, 7), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: PortStatistic(1000h) Port1 Received: 2, Sent: 3, Packets: 4, Broadcasts: 5, Multicasts: 6, Errors: 7\nEOM   : ffff0000h")
+	runMessageStringTest(t, nsdp.NewPortStatistic(1, 2, 3, 4, 5, 6, 7), "Header: 01h 02h 0000h 00000000h 00:00:00:00:00:00 00:00:00:00:00:00 0000h 0000h 4e534450h\nTLV[0]: PortStatistic(1000h) Port1 Received: 2, Sent: 3, Packets: 4, Broadcasts: 5, Multicasts: 6, Errors: 7\nEOM   : ffff0000h")
 }
 
-func runMessageMarshalingTest(t *testing.T, tlv TLV) {
+func runMessageMarshalingTest(t *testing.T, tlv nsdp.TLV) {
 	runRequestMessageMarshalingTest(t, tlv)
 	runResponseMessageMarshalingTest(t, tlv)
 }
 
-func runRequestMessageMarshalingTest(t *testing.T, tlv TLV) {
-	message1 := NewMessage(ReadRequest)
+func runRequestMessageMarshalingTest(t *testing.T, tlv nsdp.TLV) {
+	message1 := nsdp.NewMessage(nsdp.ReadRequest)
 	message1.AppendTLV(tlv)
 	marshaledBytes := message1.Marshal()
-	message2, err := UnmarshalMessage(marshaledBytes)
-	require.Nil(t, err)
+	message2, err := nsdp.UnmarshalMessage(marshaledBytes)
+	require.NoError(t, err)
 	unmarshaledBytes := message2.Marshal()
 	require.Equal(t, marshaledBytes, unmarshaledBytes)
 }
-func runResponseMessageMarshalingTest(t *testing.T, tlv TLV) {
-	message1 := NewMessage(ReadResponse)
+func runResponseMessageMarshalingTest(t *testing.T, tlv nsdp.TLV) {
+	message1 := nsdp.NewMessage(nsdp.ReadResponse)
 	message1.AppendTLV(tlv)
 	marshaledBytes := message1.Marshal()
-	message2, err := UnmarshalMessage(marshaledBytes)
-	require.Nil(t, err)
+	message2, err := nsdp.UnmarshalMessage(marshaledBytes)
+	require.NoError(t, err)
 	unmarshaledBytes := message2.Marshal()
 	require.Equal(t, marshaledBytes, unmarshaledBytes)
 }
 
-func runMessageStringTest(t *testing.T, tlv TLV, expected string) {
-	message := NewMessage(ReadResponse)
+func runMessageStringTest(t *testing.T, tlv nsdp.TLV, expected string) {
+	message := nsdp.NewMessage(nsdp.ReadResponse)
 	message.AppendTLV(tlv)
 	messageString := fmt.Sprint(message)
 	require.Equal(t, expected, messageString)
